ed2bafdf2cf44f63cd2f441e2a8e2344a4703664
yenmangu <robert.shelford@googlemail.com>
2025-04-24 17:03:03 +0100

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+function runGame() {}
+
+// Check answer function
+function checkAnswer() {}
+
+// Helper function - calculateCorrectAnswer
+function calculateCorrectAnswer() {}
+
+// Increment score/wrong answer
+function incrementScore() {}
+
+function incrementWrongAnswer() {}
+
+// Display questions
+
+function displayAdditionQuestion() {}
+function displaySubtractQuestion() {}
+function displayMultiplyQuestion() {}
+function displayDivideQuestion() {}
